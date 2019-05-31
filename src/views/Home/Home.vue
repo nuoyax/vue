7ab1738fa34f415ae588ea6922e7035a4646ebb7
Your Name <you@example.com>
2019-05-31 14:34:00 +0800

    14点33分

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,4 +1,68 @@
 <template>
+<el-container>
+  <el-header>
+    <el-row type="flex" justify="start" :gutter="4px">
+      <el-col :span="8" >
+        <div>
+          <img  src="../../assets/logo.png"/>
+        </div>
+
+      </el-col>
+      <el-col :span="8"></el-col>
+      <el-col :span="8"></el-col>
+      <el-col :span="8"></el-col>
+    </el-row>
+
+
+  </el-header>
+  <el-aside class="el-col-6">
+
+    <el-menu >
+
+
+        <el-submenu>
+          <el-menu-item-group>
+            <el-menu-item v-for="item in menu_groups" :key="item.id">{{item.value}}</el-menu-item>
+
+          </el-menu-item-group>
+
+        </el-submenu>
+
+      <el-menu-item v-for="item in one_level_menu">{{item.value}}</el-menu-item>
+    </el-menu>
+
+  </el-aside>
+<el-main>
+<el-container style="background: #d6d6d6;padding: 0px 10%">
+  <el-header>
+ <el-row :gutter="4px" type="flex" justify="start">
+   <el-col :span="4" style="text-align: left">
+     <h3>结算报表</h3>
+   </el-col>
+   <el-col :span="4"></el-col>
+   <el-col :span="4"></el-col>
+   <el-col :span="4"></el-col>
+   <el-col :span="4"></el-col>
+   <el-col :span="4"></el-col>
+
+
+ </el-row>
+  </el-header>
+  <el-main style="background: #fff;border-top: 2px solid  #c0ccda ;height: 100%">
+
+  </el-main>
+  <el-footer>@copyright 2000-0000 .</el-footer>
+
+
+</el-container>
+
+
+
+
+</el-main>
+  <el-footer></el-footer>
+</el-container>
+
 
 </template>
 
